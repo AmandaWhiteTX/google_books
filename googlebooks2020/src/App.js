fe35b8f60928c962bad0d1250214b9cc234e08c2
AmandaWhiteTX <54691794+AmandaWhiteTX@users.noreply.github.com>
2020-02-29 22:53:14 -0600

    changed header and css colors

--- googlebooks2020/src/App.js
+++ googlebooks2020/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import { HashRouter, Route, Link } from "react-router-dom";
 
 function App() {
   return (
+    <HashRouter basename='/'>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -20,7 +23,11 @@ function App() {
         </a>
       </header>
     </div>
+    </HashRouter>
   );
 }
+
+const Home = () => <div><h2>Home</h2></div>
+const About = () => <div><h2>About</h2></div>
 
 export default App;
