f15e8c3e313620c2ad8d4a78a00c795d647a4294
AmandaWhiteTX <54691794+AmandaWhiteTX@users.noreply.github.com>
2020-03-01 15:45:47 -0600

    changed footer formatting

--- js/script.jsx
+++ js/script.jsx
@@ -37,12 +37,12 @@ var Footer = React.createClass({
   render : function () {
 
     return(
-
       <div className="related">
         <a href="http://facebook.github.io/react/" target="_blank">
           <img src="img/react.png" />
           <h3>React JS</h3>
         </a>
+        
         <a href="https://developers.google.com/books/?hl=en" target="_blank">
           <img src="img/googlebooks.png" />
           <h3>Google Books API</h3>
